ba00e5c1c1893fabf5bdaa6dadfc5b82e87d1f96
Igor <igor.warlog@gmail.com>
2025-08-26 22:45:33 -0300

    Criando o botão de gegistro e suas condições.

--- main.py
+++ main.py
@@ -12,5 +12,34 @@ def main():
          COOKIE_EXPIRY_DAYS,
     )
 
+
+    if 'user_register' not in st.session_state:
+        st.session_state['user_register'] = False
+    
+    if st.session_state['user_register'] = False:
+        login_form(authenticator=authenticator)
+
+
+
+def login_form(authenticator):
+    name, authentication_status, username = authenticator.login('Login')
+    if authentication_status:
+        authenticator.logout('Logout', main)
+        st.write(f'Bem Vindo {name.title()}')
+
+
+    if authentication_status == False:
+        st.error('Usuário/senha incorreto')
+
+    if authentication_status == None:
+        st.success('Sejam bem Vindo ao Dash Boad da Engenharia')
+        user_register = st._bottom('Registrar')
+
+        if user_register:
+            st.session_state['user_register'] = True
+            st.rerun
+
+
+
 if __name__ == '__main__':
     main()
